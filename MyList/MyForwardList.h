bf2635fb45aee161a56264c39029d76e6033fed1
s3641705 <s3641705@student.rmit.edu.au>
2021-11-15 22:37:05 +1100

    c

---
+++ MyList/MyForwardList.h
@@ -0,0 +1,59 @@
+#pragma once
+#ifndef FLIST
+#define FLIST
+
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <memory>
+#include <cassert>
+#include <iterator>
+#include <type_traits>
+#include <initializer_list>
+#include <stdexcept>
+#include <vector>
+#include <list>
+
+#include "Healper.h"
+
+JSTD_START
+
+class FListNodeBase
+{
+
+public:
+
+	FListNodeBase() = default;
+
+	FListNodeBase(FListNodeBase&& other) noexcept
+		: mNext(other.mNext)
+	{
+		other.mNext = nullptr;
+	}
+
+	~FListNodeBase() = default;
+
+
+
+	FListNodeBase* mNext = nullptr;
+
+};
+
+
+class FListNode : public FListNodeBase
+{
+public:
+	FListNode();
+	~FListNode();
+
+private:
+
+};
+
+
+
+
+
+JSTD_END
+
+#endif // !FLIST
